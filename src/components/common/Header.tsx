c0d84864d54252d404478c48c9a974c1f576a062
Carlos Afonso <afonsoctw@gmail.com>
2024-08-05 15:17:57 +0100

    adicionado novo menu para cada role na navbar

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,14 +2,14 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
+import { getSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useCart } from '@/services/cart/CartContext';
 import ToggleThemeButton from './ToggleThemeButton';
 import ModalLogin from '../modals/Login';
 import Register from '../modals/Register';
-import { getSession, signOut } from 'next-auth/react';
-import type { Session } from 'next-auth';
 import { useRegister } from '@/hooks/useRegister';
-import { useRouter } from 'next/navigation';
-import { useCart } from '@/services/cart/CartContext';
+import type { Session } from 'next-auth';
 
 interface HeaderProps {
   locale: string;
@@ -18,52 +18,63 @@ interface HeaderProps {
 const Header = ({ locale }: HeaderProps) => {
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
+  const [dropdownStates, setDropdownStates] = useState({
+    accountDropdown: false,
+    userDropdown: false,
+    cartDropdown: false,
+  });
+
   const router = useRouter();
   const { cart } = useCart();
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const accountDropdownRef = useRef<HTMLDivElement>(null);
+  const userDropdownRef = useRef<HTMLDivElement>(null);
+  const cartDropdownRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const fetchSession = async () => {
       const session = await getSession();
-      if (session) {
-        setSession(session);
-      }
+      if (session) setSession(session);
       setLoading(false);
     };
     fetchSession();
   }, []);
 
   const handleOutsideClick = (event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setDropdownOpen(false);
+    if (
+      accountDropdownRef.current &&
+      !accountDropdownRef.current.contains(event.target as Node)
+    ) {
+      setDropdownStates((prev) => ({ ...prev, accountDropdown: false }));
+    }
+    if (
+      userDropdownRef.current &&
+      !userDropdownRef.current.contains(event.target as Node)
+    ) {
+      setDropdownStates((prev) => ({ ...prev, userDropdown: false }));
+    }
+    if (
+      cartDropdownRef.current &&
+      !cartDropdownRef.current.contains(event.target as Node)
+    ) {
+      setDropdownStates((prev) => ({ ...prev, cartDropdown: false }));
     }
   };
 
   useEffect(() => {
-    if (dropdownOpen) {
-      document.addEventListener('click', handleOutsideClick);
-    } else {
-      document.removeEventListener('click', handleOutsideClick);
-    }
-    return () => {
-      document.removeEventListener('click', handleOutsideClick);
-    };
-  }, [dropdownOpen]);
+    document.addEventListener('click', handleOutsideClick);
+    return () => document.removeEventListener('click', handleOutsideClick);
+  }, []);
 
   const [loginOpen, setLoginOpen] = useState(false);
   const [signupOpen, setSignupOpen] = useState(false);
-
   const handleCloseModal = () => {
     setLoginOpen(false);
     setSignupOpen(false);
   };
-
   const switchToLogin = () => {
     setSignupOpen(false);
     setLoginOpen(true);
   };
-
   const switchToSignup = () => {
     setLoginOpen(false);
     setSignupOpen(true);
@@ -85,18 +96,16 @@ const Header = ({ locale }: HeaderProps) => {
 
   if (loading) return null;
 
-  const isUserLoggedIn = session !== null;
-  const defaultAvatar = "/images/profile.png";
-  const userAvatar = isUserLoggedIn
-    ? session?.user?.image || defaultAvatar
-    : "https://www.svgrepo.com/show/157840/user.svg";
-
-  const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
-  const cartTotalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
-
+  const isUserLoggedIn = !!session;
+  const defaultAvatar = '/images/profile.png';
+  const userAvatar = session?.user?.image || defaultAvatar;
   const handleLocaleChange = (newLocale: string) => {
     window.location.href = `/${newLocale}`;
   };
+
+  const userRole = session?.user?.role;
+  const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
+  const cartTotalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
 
   return (
     <>
@@ -109,9 +118,8 @@ const Header = ({ locale }: HeaderProps) => {
             Marketplace
           </Link>
         </div>
-        <div className="flex-none flex items-center">
-          {/* Language Flags */}
-          <div className="flex space-x-2 mr-4">
+        <div className="flex-none flex items-center space-x-4">
+          <div className="flex space-x-2">
             <button onClick={() => handleLocaleChange('pt')} className="focus:outline-none">
               <img src="/icons/portugal.png" alt="Português" className="w-6 h-6" />
             </button>
@@ -123,110 +131,177 @@ const Header = ({ locale }: HeaderProps) => {
             </button>
           </div>
 
-          <div className="dropdown dropdown-end" ref={dropdownRef}>
+          {!isUserLoggedIn || (userRole !== 'seller' && userRole !== 'admin') ? (
+            <div className="dropdown dropdown-end" ref={cartDropdownRef}>
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle"
+                onClick={() => setDropdownStates((prev) => ({ ...prev, cartDropdown: !prev.cartDropdown }))}
+              >
+                <div className="indicator">
+                  <span className="icon-[mdi--cart] h-5 w-5 text-base-content"></span>
+                  <span className="badge badge-sm indicator-item bg-primary text-white">
+                    {cartItemCount}
+                  </span>
+                </div>
+              </div>
+              {dropdownStates.cartDropdown && (
+                <div
+                  tabIndex={0}
+                  className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+                >
+                  <div className="card-body">
+                    <span className="text-lg font-bold text-base-content">{cartItemCount} Itens</span>
+                    <span className="text-info">Subtotal: €{cartTotalPrice}</span>
+                    <div className="card-actions">
+                      <Link
+                        href={`/${locale}/cart`}
+                        locale={locale}
+                        className="btn btn-primary btn-block"
+                        onClick={() => setDropdownStates((prev) => ({ ...prev, cartDropdown: false }))}
+                      >
+                        Ver carrinho
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : null}
+
+          {isUserLoggedIn && (userRole === 'seller' || userRole === 'admin') && (
+            <div className="dropdown dropdown-end" ref={accountDropdownRef}>
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle"
+                onClick={() => setDropdownStates((prev) => ({ ...prev, accountDropdown: !prev.accountDropdown }))}
+              >
+                {userRole === 'seller' ? (
+                  <span className="icon-[mdi--storefront-outline] h-7 w-7 text-base-content"></span>
+                ) : (
+                  <span className="icon-[mdi--account-group-outline] h-7 w-7 text-base-content"></span>
+                )}
+              </div>
+              {dropdownStates.accountDropdown && (
+                <div
+                  tabIndex={0}
+                  className="card card-compact dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-48 p-2 shadow-md"
+                >
+                  <ul className="menu menu-compact">
+                    {userRole === 'seller' && (
+                      <li>
+                        <Link
+                          href={`/${locale}/profile/add-product`}
+                          locale={locale}
+                          className="text-sm"
+                        >
+                          Adicionar Produto
+                        </Link>
+                      </li>
+                    )}
+                    {userRole === 'admin' && (
+                      <li>
+                        <Link
+                          href={`/${locale}/admin/request-sellers`}
+                          locale={locale}
+                          className="text-sm"
+                        >
+                          Solicitações para Vendedores
+                        </Link>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
+
+          <div className="dropdown dropdown-end" ref={userDropdownRef}>
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle"
-              onClick={() => setDropdownOpen(!dropdownOpen)}
+              onClick={() => setDropdownStates((prev) => ({ ...prev, userDropdown: !prev.userDropdown }))}
             >
-              <div className="indicator">
-                <span className="icon-[mdi--cart] h-5 w-5 text-base-content"></span>
-                <span className="badge badge-sm indicator-item bg-primary text-white">{cartItemCount}</span>
-              </div>
-            </div>
-            {dropdownOpen && (
-              <div
-                tabIndex={0}
-                className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-              >
-                <div className="card-body">
-                  <span className="text-lg font-bold text-base-content">{cartItemCount} Itens</span>
-                  <span className="text-info">Subtotal: €{cartTotalPrice}</span>
-                  <div className="card-actions">
-                    <Link href={`/${locale}/cart`} locale={locale} className="btn btn-primary btn-block" onClick={() => setDropdownOpen(false)}>
-                      Ver carrinho
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
               {isUserLoggedIn ? (
                 <img alt="User Avatar" src={userAvatar} className="w-10 h-10 rounded-full" />
               ) : (
                 <span className="icon-[mdi--user] h-6 w-6 text-base-content"></span>
               )}
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-base-content">
-              {isUserLoggedIn ? (
-                <>
-                  <li>
-                    <Link href={`/${locale}/profile`} locale={locale}>
-                      Perfil
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href={`/${locale}/profile/add-product`} locale={locale}>
-                      Adicionar Produto
-                    </Link>
-                  </li>
-                  <li><a onClick={handleLogout}>Logout</a></li>
-                  <li className="my-2 border-t border-gray-200"></li>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setLoginOpen(true);
-                      }}
-                    >
-                      Iniciar Sessão
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setSignupOpen(true);
-                      }}
-                    >
-                      Registrar
-                    </a>
-                  </li>
-                  <li className="my-2 border-t border-gray-200"></li>
-                </>
-              )}
-              <li>
-                <ToggleThemeButton />
-              </li>
-            </ul>
+            {dropdownStates.userDropdown && (
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-48 p-2 shadow-md text-base-content"
+              >
+                {isUserLoggedIn ? (
+                  <>
+                    <li>
+                      <Link href={`/${locale}/profile`} locale={locale} className="text-sm">
+                        Perfil
+                      </Link>
+                    </li>
+                    <li>
+                      <a onClick={handleLogout} className="text-sm">
+                        Logout
+                      </a>
+                    </li>
+                    <li className="my-2 border-t border-gray-200"></li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <a
+                        href="#"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setLoginOpen(true);
+                        }}
+                        className="text-sm"
+                      >
+                        Iniciar Sessão
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setSignupOpen(true);
+                        }}
+                        className="text-sm"
+                      >
+                        Registrar
+                      </a>
+                    </li>
+                    <li className="my-2 border-t border-gray-200"></li>
+                  </>
+                )}
+                <li>
+                  <ToggleThemeButton />
+                </li>
+              </ul>
+            )}
           </div>
-
-          <ModalLogin
-            open={loginOpen}
-            handleCloseModal={handleCloseModal}
-            switchToSignup={switchToSignup}
-            onLoginSuccess={handleLoginSuccess}
-          />
-
-          <Register
-            open={signupOpen}
-            handleCloseModal={handleCloseModal}
-            switchToLogin={switchToLogin}
-            handleSubmit={handleSubmit}
-            message={message}
-          />
         </div>
       </header>
+
+      <ModalLogin
+        open={loginOpen}
+        handleCloseModal={handleCloseModal}
+        switchToSignup={switchToSignup}
+        onLoginSuccess={handleLoginSuccess}
+      />
+
+      <Register
+        open={signupOpen}
+        handleCloseModal={handleCloseModal}
+        switchToLogin={switchToLogin}
+        handleSubmit={handleSubmit}
+        message={message}
+      />
     </>
   );
 };
